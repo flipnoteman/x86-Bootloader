b8462af2cbe558c582368ca2cbba52c372f43873
Sam <sammys.email.home@gmail.com>
2023-05-05 14:54:13 -0500

    Modified the compile script to compile and link with linux to fix addressing issue

--- boot.s
+++ boot.s
@@ -5,31 +5,25 @@
 
 ## Preprocessor Directives
 .code16 # tells the assembler that our code needs to run in 16bit mode with 16bit registers
-.global _start # Makes our label "init" available to outside linking files
-
+.global _start # Makes our label "_start" available to outside linking files
 
 ## Text Section
 _start: # this is the beginning of our binary
-  #jmp _start  # jump to _start, this creates an infinite loop
-  # ljmpw $0xFFFF, $0 # jumps to "reset vector", re-executing the first instruction, doing a "warm-reboot"; ljmp is an instruction that means "Long Jump", you can jump to specific memory address. 
- 
- ## Print to screen
-  # mov $0x0e, %ah # moves 0xe to ah register (function teletype), ah is the bits 8-15 of the EAX register and al to 0x41 (Which is "A")
-  # mov $0x41, %al # moves 41 into the lower 8 bits of the EAX register 
-  
-  # instead of doing them seperately, you can manipulate the entire lower 16 bits of the EAX register with the ax register
-  # mov $0x0e41, %ax # represents ah + al (16 bits)
+  mov $msg, %si # Loads the first byte of msg into si 
+  mov $0xe, %ah # Loads the Teletype function into ah
 
-  # We want to write the full message, so here's how
-  mov $0x0e, %ah # Move out function teletype into ah
-  mov $msg, %bx # sets BX to the address of the first byte of our messagethe reason we go for bx is because addresses are 16 bits long, and bx is the culmination of bh and bl, two 8 bit long addresses
-  mov (%bx), %al # sets AL to the first byte of our message (() dereferences the data from that address)
-
-  int $0x10 # Call the function in ah from interrupt 0x10 http://www.ctyme.com/intr/rb-0106.htm
+print_char:
+  lodsb # Loads the byte from address in si into al and increments si
+  cmp $0, %al # Comparies content in AL with zero
+  je done # if al == 0, go to "done"
+  int $0x10 # prints the character in al to screen (calls interrupt 10)
+  jmp print_char # Repeat
+done:
   hlt # stops re-executing
 
-msg: .ascii "AHello World!" # Stores the string (plus a byte with value "0") and gives us access via $msg. The extra byte comes from the z on the and of asci, if we had used .ascii it would not have done this.
+msg: .asciz "Hello World!"
 
-## Data Section
-.fill 510-(.-_start), 1, 0 # add zeroes to make the bootloader it 510 bytes long
+.fill 510-(.-_start), 1, 0 # add 0's to make it 510 bytes long
 .word 0xaa55  # magic bytes that tell BIOS that this is bootable, putting it at the end here stores it at the end of our binary
+
+
--- compile.ps1
+++ compile.ps1
@@ -1,10 +1,11 @@
 
 # Assemble the bootloader
 write "Assembling ..."
-as -o boot.o boot.s
+# as -o boot.o boot.s
+wsl bash -c "as -o boot.o ./boot.s"
 write "Linking ..."
-ld -r -Ttext 0x7c00 -e _start -s -o boot.out boot.o
-objcopy -O binary -j .text boot.out boot.bin
+wsl bash -c "ld -o boot.bin --oformat binary -e _start -Ttext 0x7c00 -o boot.bin boot.o"
+# ld -r -Ttext 0x7c00 -e _start -o boot.out boot.o
+# objcopy -O binary -j .text boot.out boot.bin
 write "Removing output files ..."
-rm boot.out
 rm boot.o
